c665b9195be2efc4c97dcbf4101b4dcb582e3520
trevor@embedx.co <trevor@embedx.co>
2025-01-29 13:29:37 +0000

    Created slideshow; Edited settings

---
+++ client_code/slideshow/__init__.py
@@ -0,0 +1,34 @@
+from ._anvil_designer import slideshowTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class slideshow(slideshowTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    def __init__(self, **properties):
+        self.init_components(**properties)
+
+    def set_images(self, image_urls):
+        """Pass images from Python to JavaScript"""
+        js.call("setImages", image_urls)
+
+    def next(self):
+        """Move to the next slide"""
+        js.call("nextSlide")
+
+    def previous(self):
+        """Move to the previous slide"""
+        js.call("prevSlide")
+
+    def toggle_pause(self):
+        """Pause or resume the slideshow"""
+        js.call("togglePause")
+
+    # Any code you write here will run before the form opens.
