884ce105fcb5ca7f699e41947de85a8738ce0700
trevor@embedx.co <trevor@embedx.co>
2025-01-31 20:49:33 +0000

    Edited Memories.Secure

--- client_code/Memories/Secure/__init__.py
+++ client_code/Memories/Secure/__init__.py
@@ -16,8 +16,15 @@ class Secure(SecureTemplate):
 
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    pass
-
+    display_warning()
   def text_box_1_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     pass
+
+  def text_box_1_lost_focus(self, **event_args):
+    """This method is called when the TextBox loses focus"""
+    display_warning()
+
+def display_warning():
+  anvil.alert("Your embedding is now only accessible with this passcode. Please make sure this is memorable so you do not lose access to your embedded moments",title="Passcode Applied",dismissible=True)
+    
